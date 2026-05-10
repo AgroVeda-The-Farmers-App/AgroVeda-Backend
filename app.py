b188f5213bc02fed38708b1107f0de1aa658da30
Archan47 <chowdhuryarchan76@gmail.com>
2026-05-10 10:51:27 +0530

    updated app

--- app.py
+++ app.py
@@ -21,10 +21,11 @@ mail = Mail(app)
 
 CORS(app,
     supports_credentials=True,
-    origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    origins=["http://localhost:5173", "http://127.0.0.1:5173","http://localhost:5174"],
     allow_headers=["Content-Type", "Authorization"],
     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 )
+
 
 # Register blueprints
 from routes.auth import auth_bp
